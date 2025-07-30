a2107304e55d2d726a04bcb5952e66a2429668d4
Yurick <subbotinyura1980@gmail.com>
2025-07-30 03:02:21 +0300

    Прометей: Возвращение к истоку. Сброс Root Directory.

--- api/status.js
+++ api/status.js
@@ -1,27 +1,28 @@
 const pool = require('../db');
 
 module.exports = async (req, res) => {
-  try {
-    const client = await pool.connect();
-    try {
-      const result = await client.query('SELECT NOW()');
-      
-      console.log('Прометей: Рукопожатие успешно. Статус: ОНЛАЙН.');
-      res.status(200).json({ 
-        status: 'online',
-        message: 'Прометей: Рукопожатие с базой данных успешно. Система видит матрицу.',
-        time: result.rows[0].now 
-      });
+	try {
+		const client = await pool.connect();
+		try {
+			const result = await client.query('SELECT NOW()');
 
-    } finally {
-      client.release();
-    }
-  } catch (error) {
-    console.error('Прометей: КРИТИЧЕСКАЯ ОШИБКА СТАТУСА!', error);
-    res.status(500).json({ 
-      status: 'offline',
-      message: 'Прометей: КРИТИЧЕСКАЯ ОШИБКА! Не удалось установить рукопожатие с базой данных.',
-      error: error.message
-    });
-  }
+			console.log('Прометей: Рукопожатие успешно. Статус: ОНЛАЙН.');
+			res.status(200).json({
+				status: 'online',
+				message: 'Прометей: Рукопожатие с базой данных успешно. Система видит матрицу.',
+				time: result.rows[0].now
+			});
+
+		} finally {
+			client.release();
+		}
+	} catch (error) {
+		console.error('Прометей: КРИТИЧЕСКАЯ ОШИБКА СТАТУСА!', error);
+		res.status(500).json({
+			status: 'offline',
+			message: 'Прометей: КРИТИЧЕСКАЯ ОШИБКА! Не удалось установить рукопожатие с базой данных.',
+			error: error.message
+		});
+	}
 };
+//тест
